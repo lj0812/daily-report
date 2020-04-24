451f460ea1be9e955a9ccfa070d3b7ec68cbb7a0
lj0812 <xoyljlj@163.com>
2020-04-24 23:08:37 +0800

    【新增】邮件协议

--- src/constant/email-transport.js
+++ src/constant/email-transport.js
@@ -1,9 +1,29 @@
-const QQ_SMTP = {
+const EMAIL_QQ = {
   host: 'smtp.exmail.qq.com',
   port: 465,
   secure: true
 }
 
+const EMAIL_126 = {
+  host: 'smtp.126.com',
+  port: 465,
+  secure: true
+}
+
+const EMAIL_163 = {
+  host: 'smtp.163.com',
+  port: 465,
+  secure: true
+}
+
+const EMAIL_GMAIL = {
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: true
+}
+
 module.exports = {
-  QQ_SMTP
+  EMAIL_QQ,
+  EMAIL_126,
+  EMAIL_163
 }
