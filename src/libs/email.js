50102b22a961986e3903dab8dc79cce8124771a4
lj0812 <xoyljlj@163.com>
2020-04-24 23:08:43 +0800

    【更新】邮件模块

--- src/libs/email.js
+++ src/libs/email.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer')
 const { getEmailConfig } = require('./options')
 const { aesDecrypt } = require('./crypto')
+const emailTransportOpts = require('../constant/email-transport')
 
 const cryptedEmail = getEmailConfig()
 const email = Object.entries(cryptedEmail).reduce((acc, [key, value]) => {
@@ -10,10 +11,22 @@ const email = Object.entries(cryptedEmail).reduce((acc, [key, value]) => {
 function parseEmailAddress (address) {
   const reg = /(?<=@)([0-9a-zA-Z]+)(?=\.)/
   const matched = reg.exec(address)
+  if (matched.length === 0) {
+    return false
+  }
+
+  return matched[0]
 }
 
 function getTransportConfig () {
+  const emailDomainName = parseEmailAddress(email.address)
+  const emailTransport = emailTransportOpts[`EMAIL_${emailDomainName.toUpperCase()}`]
 
+  if (!emailTransport) {
+    return false
+  }
+
+  return emailTransport
 }
 
 function createTransporter () {
