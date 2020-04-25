4700f4d5620136ec4f7bcd8806a81a4f06bfdab2
lj0812 <xoyljlj@163.com>
2020-04-25 23:45:55 +0800

    【更新】完善邮件模块

--- src/libs/email.js
+++ src/libs/email.js
@@ -12,35 +12,53 @@ function parseEmailAddress (address) {
   const reg = /(?<=@)([0-9a-zA-Z]+)(?=\.)/
   const matched = reg.exec(address)
   if (matched.length === 0) {
+    // TODO
     return false
   }
 
   return matched[0]
 }
 
-function getTransportConfig () {
-  const emailDomainName = parseEmailAddress(email.address)
-  const emailTransport = emailTransportOpts[`EMAIL_${emailDomainName.toUpperCase()}`]
+function getTransportConfig (emailDomainName) {
+  emailDomainName = emailDomainName.toUpperCase()
+  const emailTransport = emailTransportOpts[`EMAIL_${emailDomainName}`]
 
   if (!emailTransport) {
+    // TODO
     return false
   }
 
-  return emailTransport
+  return {
+    ...emailTransport,
+    auth: {
+      user: email.address,
+      pass: email.password
+    }
+  }
 }
 
-function createTransporter () {
+function createTransporter (address) {
+  const domainName = parseEmailAddress(address)
+  if (!domainName) {
+    // TODO
+    return false
+  }
 
+  const transport = getTransportConfig(domainName)
+
+  return nodemailer.createTransporter(transport)
 }
 
-function sendEmail (msg) {
-  const transporter = createTransporter()
+function sendEmail (emailData) {
+  const transporter = createTransporter(email.address)
 
   transporter.sendMail({
+    from: email.address,
 
+    ...emailData
   })
 }
 
 module.exports = {
-
+  sendEmail
 }
